278bccc64514bcbeea8bc4600a471a0d3b00bdbe
FaMix <45107582+FaMix@users.noreply.github.com>
2023-06-30 00:32:55 +0200

    autowired dependency

--- src/main/java/it/uniroma3/siw/controller/CategoryController.java
+++ src/main/java/it/uniroma3/siw/controller/CategoryController.java
@@ -1,8 +1,11 @@
 package it.uniroma3.siw.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import it.uniroma3.siw.service.CategoryService;
 
 @Controller
 public class CategoryController {
